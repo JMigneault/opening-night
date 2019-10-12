b21d45107b14f0c66048f0b76038924cfc079e58
wizardofcos <mcos1231@gmail.com>
2019-10-12 13:45:06 -0400

    Added basic player movement/collision

---
+++ Opening Night/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public float speed = 500;
+
+    private float horz;
+    private float vert;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Time.deltaTime * speed * horz, Time.deltaTime * speed * vert);
+    }
+}
