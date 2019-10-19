79aef7f1b79728901997825c459f451bc96485ec
wizardofcos <mcos1231@gmail.com>
2019-10-19 14:41:50 -0400

    Added functionality for end game conditions

---
+++ Opening Night/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CollectKey ck = collision.gameObject.GetComponent<CollectKey>();
+        if (ck != null)
+            SceneManager.LoadScene("MichaelScene");
+
+
+    }
+}
