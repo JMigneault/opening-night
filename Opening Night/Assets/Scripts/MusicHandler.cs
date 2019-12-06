256e14e3e00c428e86c4a1aa526157c2b83735a0
Dainwoods <dainwoods@pacbell.net>
2019-12-05 19:48:53 -0500

    added title image and music

---
+++ Opening Night/Assets/Scripts/MusicHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Opening Night/Assets/Scripts/Title/TitleScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+
+    private SpriteRenderer SR;
+    private int spriteIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+        Invoke("UpdateFrame", 1f / 6f);
+        spriteIndex = 0;
+    }
+
+    private void UpdateFrame()
+    {
+        spriteIndex++;
+        if(spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        SR.sprite = sprites[spriteIndex];
+        Invoke("UpdateFrame", 1f / 6f);
+    }
+}
